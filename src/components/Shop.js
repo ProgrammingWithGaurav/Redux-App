1b16e3c44aebb5a347b8c79f695a3f129077db43
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-01 09:00:30 +0530

    setting up react app for redux

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy Bicycle 50 Rs</h2>
+            <button className="btn btn-primary mx-2">-</button>  
+            Add to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
