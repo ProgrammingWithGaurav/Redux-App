58354ab1b3557a175d69c7f887d507f5eda68009
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-11-01 09:37:49 +0530

    Creating a redux store

---
+++ src/state/index.js
@@ -0,0 +1,1 @@
+export * as actionCrators from './action-creators/index'
--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,11 +1,13 @@
-export default reducer = (state = 0, action) => {
-    if(action.type == 'deposit'){
+const reducer = (state = 0, action) => {
+    if(action.type === 'deposit'){
         return state + action.paylaod
     }    
-    if(action.type == 'withdraw'){
+    if(action.type === 'withdraw'){
         return state - action.paylaod
     }    
     else { 
         return state
     }
 } 
+
+export default reducer
